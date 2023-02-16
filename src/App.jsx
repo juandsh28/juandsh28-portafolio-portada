65ac6c075076e815774b47e319c8a35953b4882e
Juan <juanjodiazsh28@gmail.com>
2023-02-15 21:42:08 -0500

    cambios de links

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <div>
+        <img src="/casco.png" className="img" alt="imagen portada" />
+      </div>
+      <h1 className="titulo">Estamos trabajando en eso!!</h1>
+      <div className="info">
+        <p>Este sitio está actualmente en construcción.</p>
+        <p>Copyright © <a href="https://linktr.ee/juandsh28" target="_blank">Juan Diaz</a> 2023</p>
+      </div>
+    </div>
+  );
+}
+
+export default App
